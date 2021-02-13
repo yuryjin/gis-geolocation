f465a7e7a5b3dca14dff2e2c2436208acf849863
aquinas <aquinas1776@gmail.com>
2021-02-13 20:10:11 +0300

    init ts

---
+++ script.ts
@@ -0,0 +1,9 @@
+//declare var L: any;
+
+// @ts-ignore
+var map = L.map('map').setView([47.003670, 24.907089], 4);
+
+// @ts-ignore
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
